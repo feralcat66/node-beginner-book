3a3663d9928e264cd9fa22beca9f287c02d46a59
Rex Bergeron <rexbergeron@comcast.net>
2020-04-29 14:41:42 -0700

    minor adjustments

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const http = require('http');
 
-function onRequest(request, response) {
-  console.log('Request received.');
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.write('Hello World');
-  response.end();
+function start() {
+  function onRequest(request, response) {
+    console.log('Request received.');
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('Hello World');
+    response.end();
+  }
+  http.createServer(onRequest).listen(6543);
+  console.log('Server has started.');
 }
-http.createServer(onRequest).listen(6543);
 
-console.log('Server has started.');
+module.exports = {
+  start 
+};
+
+
+
+
