9f2b0c4cdf1da2e3b0fd80ddc2e2d795840b1b92
Rex Bergeron <rexbergeron@comcast.net>
2020-05-01 12:05:43 -0700

    make request handlers wait 10 seconds

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,11 +1,17 @@
 function start() {
-  console.log(`Request handler 'start' was called.`);
-  return `We startin' up`
+  console.log('Request handler \'start\' was called.');
+
+  function sleep(milliSeconds) {
+    const startTime = new Date().getTime();
+    while(new Date().getTime() < startTime + milliSeconds);
+  }
+  sleep(10000);
+  return 'This is the Start';
 }
 
 function upload() {
-  console.log(`Request handler 'upload was called`);
-  return `yee haw upload!`
+  console.log('Request handler \'upload\' was called');
+  return 'yee haw upload!';
 }
 
 module.exports = {
