e03308cd22f4c615472f3ec577542aa33bfb0d21
Rex Bergeron <rexbergeron@comcast.net>
2020-05-01 13:49:52 -0700

    ready to post

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ const url = require('url');
 
 function start(route, handle) {
   function onRequest(request, response) {
+    let postData = '';
     const pathname = url.parse(request.url).pathname;
     console.log('Request for ' + pathname + 'received');
 
-    route(handle, pathname, response);
+    request.setEncoding('utf8');
+
+    request.addListener('data', function(postDataChunk) {
+      postData += postDataChunk;
+      console.log("Received POST data chunk '" + postDataChunk + "'.'");
+    });
+
+    request.addListener('end', function() {
+      route(handle, pathname, response, postData);
+    });
   }
   http.createServer(onRequest).listen(6543);
   console.log('Server has started.');
@@ -15,7 +25,3 @@ function start(route, handle) {
 module.exports = {
   start 
 };
-
-
-
-
