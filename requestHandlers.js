5a6fcc1f859fe35e8b649ffae39a7a2fba09c24c
Rex Bergeron <rexbergeron@comcast.net>
2020-04-30 19:42:24 -0700

    request handlers started

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,1 +1,13 @@
+function start() {
+  console.log(`Request handler 'start' was called.`);
+}
+
+function upload() {
+  console.log(`Request handler 'upload was called`);
+}
+
+module.exports = {
+  start,
+  upload,
+};
 
