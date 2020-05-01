97b142be11f09dd1875f7328a285739bbb90a5ce
Rex Bergeron <rexbergeron@comcast.net>
2020-05-01 15:18:31 -0700

    finished!!!

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,7 +1,8 @@
 const querystring = require('querystring');
 const fs = require('fs');
+const formidable = require('formidable');
 
-function start(response, postData) {
+function start(response) {
   console.log('Request handler \'start\' was called.');
 
   const body = 
@@ -11,9 +12,9 @@ function start(response, postData) {
   'charset=UTF-8" />' +
   '</head>' +
   '<body>' +
-  '<form action="/upload" method="post">' +
-  '<textarea name="text" rows="20" cols="60"></textarea>' +
-  '<input type="submit" value="Submit text" />' +
+  '<form action="/upload" enctype="multipart/form-data" method="post">' +
+  '<input type="file" name="upload" />' +
+  '<input type="submit" value="upload file" />' +
   '</form>' +
   '</body>' +
   '</html>';
@@ -23,17 +24,32 @@ function start(response, postData) {
   response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
   console.log('Request handler \'upload\' was called');
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.write('You sent the text. ' + querystring.parse(postData).text);
-  response.end();
+
+  const form = new formidable.IncomingForm();
+  console.log('about to parse');
+  form.parse(request, function(error, fields, files) {
+    console.log('parsing done');
+
+    fs.rename(files.upload.path, '/tmp/test.png', function(error) {
+      if(error) {
+        fs.unlink('/tmp/test.png');
+        fs.rename(files.upload.path, '/tmp/test.png');
+      }
+    });
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write('received image:<br/>');
+    response.write('<img src=\'/show\' />');
+    response.end();
+  });
 }
+
 
 function show(response) {
   console.log('Request handler \'show\' was called.');
   response.writeHead(200, { 'Content-Type': 'image/png' });
-  fs.createReadStream('/tmp/test.png').pipe(response);
+  fs.createReadStream('./tmp/test.png').pipe(response);
 }
 
 module.exports = {
